b0f48c189ff0a757e485278e8595cc4a9e7107e6
Ahammad204 <ahammadcode@gmail.com>
2025-11-13 16:42:26 +0600

    create basic project setup

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { router } from './Router/Router'
+import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <div className="max-w-7xl p-2 md:p-0 mx-auto">
+      <RouterProvider router={router} />
+      <Toaster></Toaster>
+    </div>
+
+  </StrictMode>,
+)
